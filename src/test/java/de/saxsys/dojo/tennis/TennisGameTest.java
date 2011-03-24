5851172f7a33b170bcae293ae5631d230f00f9e8
Stefan Bley <stefan.bley@saxsys.de>
2011-03-24 23:22:28 +0100

    16th test: regression test player 1 wins after second advantage

--- src/test/java/de/saxsys/dojo/tennis/TennisGameTest.java
+++ src/test/java/de/saxsys/dojo/tennis/TennisGameTest.java
@@ -156,6 +156,18 @@ public class TennisGameTest {
 		assertThat(game.getScore(), is(equalTo("Advantage P2")));
 	}
 
+	@Test
+	public void getScore_returnsP1WinsAfterP1ScoresInSecondOwnAdvantage()
+			throws Exception {
+		TennisGame game = gameBetweenP1AndP2();
+		playUntilDeuce(game);
+		game.player2Scores();
+		game.player1Scores();
+		game.player1Scores();
+		game.player1Scores();
+		assertThat(game.getScore(), is(equalTo("P1 wins")));
+	}
+
 	private TennisGame gameBetweenP1AndP2() {
 		return new TennisGame("P1", "P2");
 	}
