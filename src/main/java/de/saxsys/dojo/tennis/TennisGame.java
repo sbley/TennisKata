7392c856f059544bff49a8331e6574f806fbed2e
Stefan Bley <stefan.bley@saxsys.de>
2011-03-24 22:18:42 +0100

    2nd test: fifteen love

--- src/main/java/de/saxsys/dojo/tennis/TennisGame.java
+++ src/main/java/de/saxsys/dojo/tennis/TennisGame.java
@@ -2,8 +2,17 @@ package de.saxsys.dojo.tennis;
 
 public class TennisGame {
 
+	private int player1Scores = 0;
+
 	public String getScore() {
+		if (player1Scores>0) {
+			return "Fifteen Love";
+		} 
 		return "Love all";
+	}
+
+	public void player1Scores() {
+		player1Scores++;
 	}
 
 }
--- src/test/java/de/saxsys/dojo/tennis/TennisGameTest.java
+++ src/test/java/de/saxsys/dojo/tennis/TennisGameTest.java
@@ -2,15 +2,21 @@ package de.saxsys.dojo.tennis;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
-
 
 public class TennisGameTest {
 
 	@Test
 	public void getScore_returnsLoveAllAfterGameStarts() throws Exception {
 		assertThat(new TennisGame().getScore(), is(equalTo("Love all")));
+	}
+
+	@Test
+	public void getScore_returnsFifteenLoveAfterP1Scores() throws Exception {
+		TennisGame game = new TennisGame();
+		game.player1Scores();
+		assertThat(game.getScore(), is(equalTo("Fifteen Love")));
 	}
 }
