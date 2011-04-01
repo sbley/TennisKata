84e58f38eccdf6786684f815d3b2dfe5f6056a15
Sebastian Schmeck <sebastian.schmeck@googlemail.com>
2011-04-01 02:11:00 +0200

    Refactoring, part 13: come to an end!

--- src/main/java/de/saxsys/dojo/tennis/TennisGame.java
+++ src/main/java/de/saxsys/dojo/tennis/TennisGame.java
@@ -2,9 +2,9 @@ package de.saxsys.dojo.tennis;
 
 public class TennisGame {
 
-	private static final int DISTANCE_OFFSET = 1;
+	private static final int DEUCE_SCORE_OFFSET = 1;
 
-	private static final String[] SCORES = new String[] { //
+	private static final String[] NORMAL_SCORES = new String[] { //
 	"Love", "Fifteen", "Thirty", "Forty" };
 
 	private static final String[] DEUCE_SCORES = new String[] { //
@@ -14,24 +14,6 @@ public class TennisGame {
 
 	private int scoreOfPlayerTwo = 0;
 
-	public String score() {
-
-		if (hasWinner()) {
-			if (distanceBetweenScores() < 0) {
-				return "Player One wins!";
-			} else {
-				return "Player Two wins!";
-			}
-		}
-		if (isAtLeastDeuce()) {
-			return DEUCE_SCORES[distanceBetweenScores() + DISTANCE_OFFSET];
-		}
-		if (equalScores()) {
-			return SCORES[scoreOfPlayerOne] + " All";
-		}
-		return SCORES[scoreOfPlayerOne] + " " + SCORES[scoreOfPlayerTwo];
-	}
-
 	public void playerOneScores() {
 		scoreOfPlayerOne++;
 	}
@@ -40,12 +22,34 @@ public class TennisGame {
 		scoreOfPlayerTwo++;
 	}
 
-	private boolean isAtLeastDeuce() {
-		return sumOfScores() > 5;
+	public String score() {
+		
+		if (hasWinner()) {
+			if (isPlayerOneInFront()) {
+				return "Player One wins!";
+			} else {
+				return "Player Two wins!";
+			}
+		}
+		if (isDeuceOrAdvantage()) {
+			return DEUCE_SCORES[distanceBetweenScores() + DEUCE_SCORE_OFFSET];
+		}
+		if (equalScores()) {
+			return NORMAL_SCORES[scoreOfPlayerOne] + " All";
+		}
+		return NORMAL_SCORES[scoreOfPlayerOne] + " " + NORMAL_SCORES[scoreOfPlayerTwo];
 	}
 
 	private boolean hasWinner() {
 		return sumOfScores() > 3 && Math.abs(distanceBetweenScores()) >= 2;
+	}
+
+	private boolean isPlayerOneInFront() {
+		return distanceBetweenScores() < 0;
+	}
+
+	private boolean isDeuceOrAdvantage() {
+		return sumOfScores() > 5;
 	}
 
 	private int sumOfScores() {
